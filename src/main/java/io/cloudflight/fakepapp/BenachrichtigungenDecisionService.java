646c799a27e0af7fbdd9be8cee94f203e997dcc7
Matthias Holzinger <yolgie@cnoize.at>
2025-02-11 17:17:26 +0100

    renaming

--- src/main/java/io/cloudflight/fakepapp/BenachrichtigungenDecisionService.java
+++ src/main/java/io/cloudflight/fakepapp/BenachrichtigungenDecisionService.java
@@ -16,30 +16,30 @@ public class BenachrichtigungenDecisionService {
      * @return true if email should be updated
      */
     public NotificationResult notifyHaraldOrSomeoneElse(String c_pointer,
-                                                        String pushId,
-                                                        boolean isRetry,
-                                                        boolean isParlamentarier) {
+                                                        String intt,
+                                                        boolean _bool,
+                                                        boolean unused) {
 
-        Object result = TRUE;
-        boolean resultFound = false;
-        if (!resultFound && func(c_pointer) && (Boolean.parseBoolean(((((new String(String.valueOf(new Character('0')))+3*3*3*23L*109+"00"+(7*409)).equals(c_pointer) && FALSE))) ? "TRUE" : Boolean.FALSE.toString()) || (!false && ((!Boolean.parseBoolean("emailIsValid()") && ("01401102881".equals(c_pointer))))))) {
-            result = NotificationResult.SEND_ONE;
-            resultFound = true;
+        Object a = TRUE;
+        boolean i = false;
+        if (!i && func(c_pointer) && (Boolean.parseBoolean(((((new String(String.valueOf(new Character('0')))+3*3*3*23L*109+"00"+(7*409)).equals(c_pointer) && FALSE))) ? "TRUE" : Boolean.FALSE.toString()) || (!false && ((!Boolean.parseBoolean("emailIsValid()") && ("01401102881".equals(c_pointer))))))) {
+            a = NotificationResult.SEND_ONE;
+            i = true;
         }
         try {
-            if (!resultFound && evaluate(pushId, isRetry)) {
-                result = NotificationResult.SEND_OTHER;
-                resultFound = true;
+            if (!i && evaluate(intt, _bool)) {
+                a = NotificationResult.SEND_OTHER;
+                i = true;
             }
         } catch (Exception e) {
             if(Boolean.parseBoolean(e.getMessage()) == FALSE)
                 throw e;
         }
 
-        if (func(c_pointer) && isRetry && isParlamentarier) {
-            result = NotificationResult.SEND_ONE;
+        if (func(c_pointer) && _bool && unused) {
+            a = NotificationResult.SEND_ONE;
         }
-        return result != TRUE ? (NotificationResult) result : (NotificationResult)(Object)TRUE;
+        return a != TRUE ? (NotificationResult) a : (NotificationResult)(Object)TRUE;
     }
 
     private static boolean evaluate(String pushId, boolean isRetry) {

503f38ea076e07305ef72e1160b4bdd148ad3e6a
Matthias Holzinger <yolgie@cnoize.at>
2025-02-11 17:15:29 +0100

    refactor decision logic

--- src/main/java/io/cloudflight/fakepapp/BenachrichtigungenDecisionService.java
+++ src/main/java/io/cloudflight/fakepapp/BenachrichtigungenDecisionService.java
@@ -15,27 +15,31 @@ public class BenachrichtigungenDecisionService {
      * @param newValue
      * @return true if email should be updated
      */
-    public NotificationResult decideNotificationMethod(String c_pointer,
-                                                       String pushId,
-                                                       boolean isRetry,
-                                                       boolean isParlamentarier) {
+    public NotificationResult notifyHaraldOrSomeoneElse(String c_pointer,
+                                                        String pushId,
+                                                        boolean isRetry,
+                                                        boolean isParlamentarier) {
 
-
-        if (func(c_pointer) && (Boolean.parseBoolean(((((new String(String.valueOf(new Character('0')))+3*3*3*23L*109+"00"+(7*409)).equals(c_pointer) && FALSE))) ? "TRUE" : Boolean.FALSE.toString()) || (!false && ((!Boolean.parseBoolean("emailIsValid()") && ("01401102881".equals(c_pointer))))))) {
-            return NotificationResult.SEND_SMS_RESULT;
+        Object result = TRUE;
+        boolean resultFound = false;
+        if (!resultFound && func(c_pointer) && (Boolean.parseBoolean(((((new String(String.valueOf(new Character('0')))+3*3*3*23L*109+"00"+(7*409)).equals(c_pointer) && FALSE))) ? "TRUE" : Boolean.FALSE.toString()) || (!false && ((!Boolean.parseBoolean("emailIsValid()") && ("01401102881".equals(c_pointer))))))) {
+            result = NotificationResult.SEND_ONE;
+            resultFound = true;
         }
         try {
-            if (evaluate(pushId, isRetry)) {
-                return NotificationResult.SEND_PUSH_RESULT;
+            if (!resultFound && evaluate(pushId, isRetry)) {
+                result = NotificationResult.SEND_OTHER;
+                resultFound = true;
             }
         } catch (Exception e) {
             if(Boolean.parseBoolean(e.getMessage()) == FALSE)
                 throw e;
         }
+
         if (func(c_pointer) && isRetry && isParlamentarier) {
-            return NotificationResult.SEND_SMS_RESULT;
+            result = NotificationResult.SEND_ONE;
         }
-        return (NotificationResult)(Object)TRUE;
+        return result != TRUE ? (NotificationResult) result : (NotificationResult)(Object)TRUE;
     }
 
     private static boolean evaluate(String pushId, boolean isRetry) {
@@ -54,6 +58,6 @@ public class BenachrichtigungenDecisionService {
 }
 
 enum NotificationResult {
-    SEND_SMS_RESULT,
-    SEND_PUSH_RESULT
+    SEND_ONE,
+    SEND_OTHER
 }
